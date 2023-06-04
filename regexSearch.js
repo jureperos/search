2a23ea7e188fd2c80d4f1f1918e3bdf8dc7a5520
jureperos <jure.peros@gmail.com>
2023-06-04 09:02:36 +0200

    renamed file, made data file

---
+++ regexSearch.js
@@ -0,0 +1,40 @@
+import {RastlineArr} from './data.js'
+
+console.log(RastlineArr[1])
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let sTerm = '';
+
+rl.question('search term: ', (searchTerm) => {
+    sTerm = searchTerm;
+    console.log(`this is you search term: ${sTerm}`);
+    rl.close();
+    regSearch();
+});
+
+
+function regSearch() {
+
+    const regex = new RegExp(sTerm, 'i');
+    console.log(regex) 
+
+    for (let i = 0; i <= RastlineArr.length -1; i++) {
+        
+        if (regex.test(RastlineArr[i].title)) {
+            console.log(RastlineArr[i].title);
+        }
+        if (regex.test(RastlineArr[i].sciName)) {
+            console.log(RastlineArr[i].sciName);
+        }
+        if (regex.test(RastlineArr[i].description)) {
+            console.log(RastlineArr[i].description);
+        }
+    }
+
+}
